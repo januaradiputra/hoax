ff044dcb7cb31d0cee932d806062332ee5e2e83e
januaradiputra <72774941+januaradiputra@users.noreply.github.com>
2020-10-15 01:06:22 +0700

    Update views.py

--- oke/apps/endpoints/views.py
+++ oke/apps/endpoints/views.py
@@ -93,7 +93,7 @@ class PredictView(views.APIView):
        
         alg_index = 0
         algorithm_object = PhisingClassifier()
-        input_data=json.dumps(request.data)
+        input_data=json.loads(request.data)
         prediction = algorithm_object.prediksi(input_data['url'])
         
         
